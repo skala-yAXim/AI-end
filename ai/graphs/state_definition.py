912de193228bcfa91c9136018d2e45f78368b0b4
Kim Se Eun <rlatpdms1014@gmail.com>
2025-06-23 10:41:14 +0900

    Revert "feat: [#115] team weekly 에이전트 고도화"

--- ai/graphs/state_definition.py
+++ ai/graphs/state_definition.py
@@ -38,8 +38,7 @@ class TeamWeeklyLangGraphState(Dict):
     start_date: str
     end_date: str
     team_members: List[str]
-    # TODO project 여러개 들어오게끔 수정
-    project_id: int
+    project_id: str
     project_name: str
     project_description: str
     project_period: str
@@ -57,8 +56,7 @@ class WeeklyLangGraphState(Dict):
     user_id: str
     start_date: str
     end_date: str
-    # TODO project 여러개 들어오게끔 수정
-    project_id: int
+    project_id: str
     project_name: str
     project_description: str
     project_period: str
@@ -73,8 +71,7 @@ class ProgressSummaryState(Dict):
     github_email: Optional[str] = None 
     target_date: Optional[str] = None 
     date: str
-    # TODO project 여러개 들어오게끔 수정
-    project_id: int
+    project_id: str
     daily_report_path: str
     daily_report: Dict[str, Any]
     wbs_data: List[Dict[str, Any]]
